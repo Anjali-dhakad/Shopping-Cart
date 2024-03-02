68159f834e0f5f3bb8e9b8a476874a60349b27e5
Anjali-dhakad <anjalidhakad027@gmail.com>
2024-03-02 12:03:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Products from './component/Products';
+import Product from './component/Product';
+import About from './component/About';
+import Contact from './component/Contact';
+import Cart from './component/Cart';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/products" element={<Products/>} />
+        <Route exact path="/products/:id" element={<Product/>} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/contact" element={<Contact/>} />
+        <Route exact path="/cart" element={<Cart/>} />
+      </Routes>
+    
+    </>
+  );
+}
+
+export default App;
